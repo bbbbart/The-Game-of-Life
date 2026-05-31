662e1f3c04c62153216898a3afb299ff00a13723
bdabkowski1 <bdabkowski353@gmail.com>
2026-05-31 17:53:26 -0500

    Fix counting issues

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import time
+import copy
 
 grid1 = [["." for y in range(30)] for x in range (30)]
 
-grid1[15][15] = "■"
-grid1[15][16] = "■"
-grid1[15][17] = "■"
-grid1[16][17] = "■"
-grid1[17][16] = "■"
+grid1[3][16] = "■"
+grid1[3][17] = "■"
+grid1[3][18] = "■"
+grid1[4][18] = "■"
+grid1[5][17] = "■"
+
 
 def countNeighbors(y, x):
     count = 0
-    if y == 30 or y == 0 or x == 30 or x == 0:
+    if y == 29 or y == 0 or x == 29 or x == 0:
         return -1
     else:
         if grid1[y + 1][x] == "■":
@@ -32,25 +34,28 @@ def countNeighbors(y, x):
         return count
 
 def updateGrid(g):
-    grid2 = [["." for y in range(30)] for x in range (30)]
-    r = -1
-    c = -1
+    grid2 = copy.deepcopy(g)
+    r = 0
+    c = 0
     for row in g:
-        r+=1
-        for element in g:
-            c+=1
+        for element in row:
             count = countNeighbors(r, c)
             if count <= 1:
                 grid2[r][c] = "."
-            elif count >= 4:
+            if count > 3:
                 grid2[r][c] = "."
-            elif element == "■" and 2 <= count <=3:
+            if element == "■" and 2 <= count <=3:
                 grid2[r][c] = "■"
+            if element == "." and count == 3:
+                grid2[r][c] = "■"
+            c+=1
+        c = 0
+        r+=1
     return grid2
 
 for i in range(10):
     for row in grid1:
         print(" ".join(row))
+    print("\n")
     time.sleep(1)
-    print("\033[H\033[2J", end="")
     grid1 = updateGrid(grid1)
